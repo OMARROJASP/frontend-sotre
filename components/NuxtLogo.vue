a20e39ca79e98c67d9aebf0eba3b0b04b99c1972
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2025-05-06 19:37:06 -0500

    end footer

--- components/NuxtLogo.vue
+++ components/NuxtLogo.vue
@@ -1,11 +1,155 @@
 <template>
-  <svg class="nuxt-logo" viewBox="0 0 45 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M24.7203 29.704H41.1008C41.6211 29.7041 42.1322 29.5669 42.5828 29.3061C43.0334 29.0454 43.4075 28.6704 43.6675 28.2188C43.9275 27.7672 44.0643 27.2549 44.0641 26.7335C44.0639 26.2121 43.9266 25.6999 43.6662 25.2485L32.6655 6.15312C32.4055 5.70162 32.0315 5.32667 31.581 5.06598C31.1305 4.8053 30.6195 4.66805 30.0994 4.66805C29.5792 4.66805 29.0682 4.8053 28.6177 5.06598C28.1672 5.32667 27.7932 5.70162 27.5332 6.15312L24.7203 11.039L19.2208 1.48485C18.9606 1.03338 18.5864 0.658493 18.1358 0.397853C17.6852 0.137213 17.1741 0 16.6538 0C16.1336 0 15.6225 0.137213 15.1719 0.397853C14.7213 0.658493 14.3471 1.03338 14.0868 1.48485L0.397874 25.2485C0.137452 25.6999 0.000226653 26.2121 2.8053e-07 26.7335C-0.000226092 27.2549 0.136554 27.7672 0.396584 28.2188C0.656614 28.6704 1.03072 29.0454 1.48129 29.3061C1.93185 29.5669 2.44298 29.7041 2.96326 29.704H13.2456C17.3195 29.704 20.3239 27.9106 22.3912 24.4118L27.4102 15.7008L30.0986 11.039L38.1667 25.0422H27.4102L24.7203 29.704ZM13.0779 25.0374L5.9022 25.0358L16.6586 6.36589L22.0257 15.7008L18.4322 21.9401C17.0593 24.2103 15.4996 25.0374 13.0779 25.0374Z" fill="#00DC82" />
-  </svg>
+  <div class="container-footer">
+    <!-- Para Desktop -->
+    <!-- Para Mobile -->
+    <div class="container-footer-sup">
+      <div class="footer-logo">
+        <p class="text-orange">
+          Rukanas
+          <span class="text-white">
+            Store
+          </span>
+        </p>
+      </div>
+      <div class="footer-menu">
+        <div class="container-menu-item-title" @click="toggleOptions(1)">
+          <div class="title">
+            Enlace de Navegacion
+          </div>
+          <div class="icon-plus">
+            <font-awesome-icon :icon="['fas', 'plus']" size="2xs" style="color: #FFD43B;" />
+          </div>
+        </div>
+        <!-- Para desktop  -->
+        <div class="container-menu-item">
+          <div>
+            <nuxt-link to="/productos" class="menu-item">
+              Productos
+            </nuxt-link>
+          </div>
+          <div>
+            <nuxt-link to="/ofertas" class="menu-item">
+              Ofertas
+            </nuxt-link>
+          </div>
+          <div>
+            <nuxt-link to="/nosotros" class="menu-item">
+              Nosotros
+            </nuxt-link>
+          </div>
+          <div>
+            <nuxt-link to="/contactos" class="menu-item">
+              Contacto
+            </nuxt-link>
+          </div>
+        </div>
+        <!-- Para mobile  -->
+        <div :class="['container-menu-item-mobile', { 'container-menu-item-mobile--visible': showOptions === 1 }]">
+          <div>
+            <nuxt-link to="/productos" class="menu-item">
+              Productos
+            </nuxt-link>
+          </div>
+          <div>
+            <nuxt-link to="/ofertas" class="menu-item">
+              Ofertas
+            </nuxt-link>
+          </div>
+          <div>
+            <nuxt-link to="/nosotros" class="menu-item">
+              Nosotros
+            </nuxt-link>
+          </div>
+          <div>
+            <nuxt-link to="/contactos" class="menu-item">
+              Contacto
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <!-- Para los contactos-->
+      <div class="footer-menu">
+        <p class="container-menu-item-title" @click="toggleOptions(2)">
+          Contacto
+        </p>
+        <div>
+          <div class="footer-menu-contact-item">
+            <font-awesome-icon :icon="['fas', 'map-location-dot']" style="color: #ffffff;" />
+            <p class="footer-menu-p">Av. el sol s/n - Lucanas </p>
+          </div>
+          <div class="footer-menu-contact-item">
+            <font-awesome-icon :icon="['fas', 'phone']" style="color: #ffffff;" />
+            <p class="footer-menu-p">991117845 </p>
+          </div>
+          <div class="footer-menu-contact-item">
+            <font-awesome-icon :icon="['fas', 'envelope']" style="color: #ffffff;" />
+            <p class="footer-menu-p">
+              rukanas@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Para los terminos legales-->
+      <div class="footer-menu">
+        <p class="container-menu-item-title" @click="toggleOptions(3)">
+          Enlace Legales
+        </p>
+        <div class="container-menu-item">
+          <div class="menu-item">
+            Términos y condiciones
+          </div>
+          <div class="menu-item">
+            Politicas de privacidad
+          </div>
+        </div>
+      </div>
+      <!-- Para las redes sociales-->
+      <div class="footer-menu">
+        <p class="container-menu-item-title">
+          Síguenos en
+        </p>
+        <div class="container-menu-item">
+          <font-awesome-icon :icon="['fab', 'facebook']" style="color: #ffffff;" />
+          <font-awesome-icon :icon="['fab', 'youtube']" />
+          <font-awesome-icon :icon="['fab', 'x-twitter']" />
+          <font-awesome-icon :icon="['fab', 'tiktok']" />
+        </div>
+      </div>
+      <!-- Para el págo de tarjetas-->
+      <div class="footer-menu">
+        <p class="container-menu-item-title">
+          Medios de Pago
+        </p>
+        <div>
+          <img src="@/assets/img/footer/paga-aqui.png" class="footer-pago-footer"/>
+        </div>
+      </div>
+    </div>
+    <div>
+      <p class="container-footer-inf">
+        © 2023 Rukanas Store. Todos los derechos reservados.
+      </p>
+    </div>
+  </div>
 </template>
-
-<style>
-.nuxt-logo {
-  height: 180px;
+<script>
+export default {
+  name: 'CommonFooter',
+  data () {
+    return {
+      lugar: 'Lima',
+      showOptions: 0
+    }
+  },
+  methods: {
+    // Add any methods you need here
+    toggleOptions (option) {
+      if (option === this.showOptions) {
+        this.showOptions = 0
+      } else {
+        this.showOptions = option
+      }
+    }
+  }
 }
-</style>
+</script>
--- components/common/CommonFooter.vue
+++ components/common/CommonFooter.vue
@@ -1,6 +1,156 @@
 <template>
-  <div class="container-footer">
-    <div class="container-footer-sup">
+  <div class="footer">
+    <!-- Para Desktop -->
+    <div class="footer__sup">
+      <!-- Section Logo -->
+      <div class="section">
+        <div class="section__logo">
+          <p class="text-orange">
+            Rukanas
+            <span class="text-white">
+              Store
+            </span>
+          </p>
+        </div>
+      </div>
+      <!-- Section Enlace de Navegacion-->
+      <div class="section__options">
+        <div :class="['section__options__title', { 'section__options__title--dark': showOptions === 1 }]" @click="toggleOptions(1)">
+          <div class="title">
+            Enlace de Navegacion
+          </div>
+          <div class="icon-plus">
+            <font-awesome-icon :icon="['fas', showOptions === 1 ?'minus':'plus']" size="2xs" style="color: #FFD43B;" />
+          </div>
+        </div>
+        <div :class="['section__options__body', { 'section__options__body--visible': showOptions === 1 }]">
+          <ul class="list-options">
+            <li class="option">
+              <nuxt-link to="/productos" class="option__menu-item">
+                Productos
+              </nuxt-link>
+            </li>
+            <li class="option">
+              <nuxt-link to="/ofertas" class="option__menu-item">
+                Ofertas
+              </nuxt-link>
+            </li>
+            <li class="option">
+              <nuxt-link to="/nosotros" class="option__menu-item">
+                Nosotros
+              </nuxt-link>
+            </li>
+            <li class="option">
+              <nuxt-link to="/contactos" class="option__menu-item">
+                Contacto
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- Section Contacto -->
+      <div class="section__options">
+        <div :class="['section__options__title', { 'section__options__title--dark': showOptions === 2 }]" @click="toggleOptions(2)">
+          <div class="title">
+            Contacto
+          </div>
+          <div class="icon-plus">
+            <font-awesome-icon :icon="['fas', showOptions === 2 ?'minus':'plus']" size="2xs" style="color: #FFD43B;" />
+          </div>
+        </div>
+        <div :class="['section__options__body', { 'section__options__body--visible': showOptions === 2 }]">
+          <ul class="list-options">
+            <li class="option__contact-item">
+              <font-awesome-icon :icon="['fas', 'map-location-dot']" style="color: #ffffff;" class="img" />
+              <p class="footer-menu-p">Av. el sol s/n - Lucanas </p>
+            </li>
+            <li class="option__contact-item">
+              <font-awesome-icon :icon="['fas', 'phone']" style="color: #ffffff;" class="img" />
+              <p class="footer-menu-p">991117845 </p>
+            </li>
+            <li class="option__contact-item">
+              <font-awesome-icon :icon="['fas', 'envelope']" style="color: #ffffff;" class="img" />
+              <p class="footer-menu-p">
+                rukanas@gmail.com
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- Section Enlaces Legales -->
+      <div class="section__options">
+        <div :class="['section__options__title', { 'section__options__title--dark': showOptions === 3 }]" @click="toggleOptions(3)">
+          <div class="title">
+            Enlace Legales
+          </div>
+          <div class="icon-plus">
+            <font-awesome-icon :icon="['fas', showOptions === 3 ?'minus':'plus']" size="2xs" style="color: #FFD43B;" />
+          </div>
+        </div>
+        <div :class="['section__options__body', { 'section__options__body--visible': showOptions === 3 }]">
+          <ul class="list-options">
+            <li class="option">
+              <p class="option__menu-item">
+                Términos y condiciones
+              </p>
+            </li>
+            <li class="option">
+              <p class="option__menu-item">
+                Politicas de privacidad
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- Section Redes Sociales -->
+      <div class="section__options">
+        <div :class="['section__options__title', { 'section__options__title--dark': showOptions === 4 }]" @click="toggleOptions(4)">
+          <div class="title">
+            Siguenos en
+          </div>
+          <div class="icon-plus">
+            <font-awesome-icon :icon="['fas', showOptions === 4 ?'minus':'plus']" size="2xs" style="color: #FFD43B;" />
+          </div>
+        </div>
+        <div :class="['section__options__body', { 'section__options__body--visible': showOptions === 4 }]">
+          <ul class="list-options">
+            <li class="option">
+              <font-awesome-icon :icon="['fab', 'facebook']" style="color: #ffffff;" />
+            </li>
+            <li class="option">
+              <font-awesome-icon :icon="['fab', 'youtube']" />
+            </li>
+            <li class="option">
+              <font-awesome-icon :icon="['fab', 'x-twitter']" />
+            </li>
+            <li class="option">
+              <font-awesome-icon :icon="['fab', 'tiktok']" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- Section Paga con Tarjetas -->
+      <div class="section__options">
+        <div class="section__options__title--none">
+          <div class="title">
+            Medios de Pago
+          </div>
+        </div>
+        <div :class="['section__options__body', { 'section__options__body--visible': true }]">
+          <div class="list-options">
+            <div class="option">
+              <img src="@/assets/img/footer/paga-aqui.png" class="option__pago-item" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Section Inf footer-->
+    <div class="footer__inf">
+      © 2023 Rukanas Store. Todos los derechos reservados.
+    </div>
+    <!-- Para Mobile -->
+    <!-- <div class="container-footer-sup">
       <div class="footer-logo">
         <p class="text-orange">
           Rukanas
@@ -40,7 +190,6 @@
             </nuxt-link>
           </div>
         </div>
-        <!-- Para mobile  -->
         <div :class="['container-menu-item-mobile', { 'container-menu-item-mobile--visible': showOptions === 1 }]">
           <div>
             <nuxt-link to="/productos" class="menu-item">
@@ -65,7 +214,7 @@
         </div>
       </div>
       <div class="footer-menu">
-        <p class="container-menu-item-title">
+        <p class="container-menu-item-title" @click="toggleOptions(2)">
           Contacto
         </p>
         <div>
@@ -86,7 +235,7 @@
         </div>
       </div>
       <div class="footer-menu">
-        <p class="container-menu-item-title">
+        <p class="container-menu-item-title" @click="toggleOptions(3)">
           Enlace Legales
         </p>
         <div class="container-menu-item">
@@ -98,6 +247,7 @@
           </div>
         </div>
       </div>
+
       <div class="footer-menu">
         <p class="container-menu-item-title">
           Síguenos en
@@ -122,7 +272,7 @@
       <p class="container-footer-inf">
         © 2023 Rukanas Store. Todos los derechos reservados.
       </p>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
