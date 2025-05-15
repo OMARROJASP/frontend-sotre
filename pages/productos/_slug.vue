c91be34d2054f62d8a4a201a4482267b459554b3
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2025-05-15 00:12:52 -0500

    diseño de detalle product

--- pages/productos/_slug.vue
+++ pages/productos/_slug.vue
@@ -1,25 +1,32 @@
 <template>
-  <div id="page-product">
-    <div class="container">
-      <div class="content">
-          <div>
-        <img :src="product.prod_imageUrl"/>
-      </div>
-      <div>
-        <h3>{{ product.prod_name }}</h3>
-        <h4>{{ product.prod_description }}</h4>
-        <h3>S/ {{ product.prod_price }}</h3>
-        <h3>Stock {{ product.prod_stock }}</h3>
-      </div>
+  <div>
+    <CommonHeader />
+    <div id="page-product">
+      <div class="container">
+        <div class="content">
+          <div class="content__img">
+            <img :src="product.prod_imageUrl"/>
+          </div>
+          <div class="content__info">
+            <h3>{{ product.prod_name }}</h3>
+            <h4>{{ product.prod_description }}</h4>
+            <h3>S/ {{ product.prod_price }}</h3>
+            <h3>Stock {{ product.prod_stock }}</h3>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex'
+import CommonHeader from '~/components/common/CommonHeader.vue'
 
 export default {
   name: 'ProductPage',
+  components: {
+    CommonHeader
+  },
   data () {
     return {
       idProduct: ''
